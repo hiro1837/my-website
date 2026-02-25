7f6c911ed11e25a7a065e2b9e9eba674534ea661
hiro1837 <hirok5586@gmail.com>
2026-02-25 21:15:34 +0900

    Setup GitHub Actions for automatic deployment and fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
 
     return {
+        base: '/my-website/',
         plugins: [react()],
         resolve: {
             alias: {
